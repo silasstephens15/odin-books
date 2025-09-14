b5bfb6c29e40d9d48736a9fabea6466a02be70bf
Silas Stephens <76085257+silasstephens15@users.noreply.github.com>
2025-09-14 18:35:31 -0400

    Books now display an info card.
    This will provide the basis of the GUI for the library.

--- library.js
+++ library.js
@@ -9,4 +9,24 @@ function Book(name, read) {
 function addBookToLibrary(name, read) {
   let newBook = new Book(name, read);
   library.push(newBook);
+
+  const container = document.querySelector(".card-container");
+  const card = document.createElement("div");
+  card.classList.add("card");
+  const title = document.createElement("h2");
+  title.textContent = name;
+  const wasRead = document.createElement("p");
+  if (read) {
+    wasRead.textContent = "Read";
+  } else {
+    wasRead.textContent = "Not yet read";
+  }
+  const button = document.createElement("button");
+  button.id = newBook.id;
+  button.textContent = "Delete";
+
+  container.appendChild(card);
+  card.appendChild(title);
+  card.appendChild(wasRead);
+  card.appendChild(button);
 }
